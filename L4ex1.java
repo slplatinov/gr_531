f56d1af5d2a9f661d7c750831d70f74f813fbee0
SergeyPlatinov <slplatinov@yandex.ru>
2022-06-09 22:20:57 +0300

    Create laboratory work №4 ex1

--- L4ex1.java
+++ L4ex1.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class L4ex1 {
+public class L4ex1 {
+    public static void main(String[] args) {
+        int minNum;
+        int maxNum;
+        int multNum;
+        int i;
+        minNum = 1;
+        maxNum = 10;
+        multNum = 7;
+        for (i=1;i<=10;i++)
+        {
+            System.out.println(i + " * " + multNum + " = " + i*multNum);
+        }
+    }
 }
