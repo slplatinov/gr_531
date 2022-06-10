21de8b42a15cf823ba55c81a5922f934ee02cc90
SergeyPlatinov <slplatinov@yandex.ru>
2022-06-10 12:22:22 +0300

    Create laboratory work №5 ex1

---
+++ L5ex1.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class L5ex1 {
+    public static void main(String[] args) {
+        Person person1 = new Person("King Lion",20);
+        Person person2 = new Person("Smoll Boy",10);
+        System.out.println(person1.fullName + " is age - " + person1.age);
+        System.out.println(person2.fullName + " is age - " + person2.age);
+        person1.say(person2.fullName);
+        person1.move(person2.age);
+    }
+}
---
+++ Person.java
@@ -0,0 +1,25 @@
+import java.awt.desktop.ScreenSleepEvent;
+
+public class Person {
+    String fullName;
+    int age;
+
+
+    Person (String namePerson, int age)
+    {
+        this.fullName = namePerson;
+        this.age = age;
+
+    }
+
+    static void say(String namePerson)
+    {
+        System.out.println("Person " + namePerson + " say:-- Hello!!!");
+
+    }
+
+    static void move(int age)
+    {
+        System.out.println("Person of age is age - " + age + "It's is very young.");
+    }
+}
